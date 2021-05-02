a618204cbf5af50d0bb59dc6231abfaa4e787761
Yegua Celestial <yeguacelestial@gmail.com>
2021-05-02 17:52:01 -0500

    Fix some bugs, and read images without downloading them

--- tinder_bot.py
+++ tinder_bot.py
@@ -7,11 +7,12 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
 
 from time import sleep
 from dotenv import load_dotenv
 
-from use_model import predict_from_img_path
+from use_model import predict_from_img_path, predict_from_url_path
 
 load_dotenv()
 
@@ -41,16 +42,20 @@ class TinderBot():
 
         driver.get('http://tinder.com')
 
+        # Get elements IDs
+        self.main_div_id = driver.find_element_by_xpath('//body/div[1]').get_attribute('id')
+        self.alert_div_id = driver.find_element_by_xpath('//body/div[2]').get_attribute('id')
+
         # Accept terms
-        driver.find_element_by_xpath('//*[@id="t-1801132545"]/div/div[2]/div/div/div[1]/button').click()
+        driver.find_element_by_xpath(f'//*[@id="{self.main_div_id}"]/div/div[2]/div/div/div[1]').click()
         sleep(random_delay())
 
         # Login button
-        driver.find_element_by_xpath('//*[@id="t-1801132545"]/div/div[1]/div/main/div[1]/div/div/div/div/header/div/div[2]/div[2]/button').click()
+        driver.find_element_by_xpath(f'//*[@id="{self.main_div_id}"]/div/div[1]/div/main/div[1]/div/div/div/div/header/div/div[2]/div[2]/a').click()
         sleep(random_delay())
 
         # Login with phone number
-        driver.find_element_by_xpath('//*[@id="t--239073259"]/div/div/div[1]/div/div[3]/span/div[3]/button').click()
+        driver.find_element_by_xpath(f'//*[@id="{self.alert_div_id}"]/div/div/div[1]/div/div[3]/span/div[2]/button').click()
         sleep(random_delay())
 
         # Try to wait for captcha modal to appear...
@@ -62,44 +67,44 @@ class TinderBot():
             pass
         
         # Introduce phone number
-        driver.find_element_by_xpath('//*[@id="t--239073259"]/div/div/div[1]/div[2]/div/input').send_keys(os.getenv('PHONE_NUMBER'))
+        driver.find_element_by_xpath(f'//*[@id="{self.alert_div_id}"]/div/div/div[1]/div[2]/div/input').send_keys(os.getenv('PHONE_NUMBER'))
         sleep(random_delay())
 
-        # Button "send sms"
-        driver.find_element_by_xpath('//*[@id="t--239073259"]/div/div/div[1]/button').click()
+        # Button "Continue"
+        driver.find_element_by_xpath(f'//*[@id="{self.alert_div_id}"]/div/div/div[1]/button/span').click()
         sleep(random_delay())
 
         # Ask for phone code
-        sms_code = input("SMS Code: ")
+        sms_code = input("[*********] SMS Code -> ")
 
         # SMS code inputs on each case
         for i in range(0,6):
-            driver.find_element_by_xpath(f'//*[@id="t--239073259"]/div/div/div[1]/div[3]/input[{i+1}]').send_keys(sms_code[i])
+            driver.find_element_by_xpath(f'//*[@id="{self.alert_div_id}"]/div/div/div[1]/div[3]/input[{i+1}]').send_keys(sms_code[i])
 
         sleep(random_delay())
 
         # Continue button
-        driver.find_element_by_xpath('//*[@id="t--239073259"]/div/div/div[1]/button').click()
+        driver.find_element_by_xpath(f'//*[@id="{self.alert_div_id}"]/div/div/div[1]/button').click()
         sleep(random_delay())
 
         # Email code handling
-        email_code = input("Email code: ")
+        email_code = input("[**********] Email code -> ")
 
         for i in range(0,6):
-            driver.find_element_by_xpath(f'//*[@id="t--239073259"]/div/div/div[1]/div[3]/input[{i+1}]').send_keys(email_code[i])
+            driver.find_element_by_xpath(f'//*[@id="{self.alert_div_id}"]/div/div/div[1]/div[3]/input[{i+1}]').send_keys(email_code[i])
         
         sleep(random_delay())
 
         # Next button
-        driver.find_element_by_xpath('//*[@id="t--239073259"]/div/div/div[1]/button').click()
+        driver.find_element_by_xpath(f'//*[@id="{self.alert_div_id}"]/div/div/div[1]/button').click()
         sleep(random_delay())
 
         # Try to accept notifications and stuff
         try:
-            driver.find_element_by_xpath('//*[@id="t--239073259"]/div/div/div/div/div[3]/button[1]').click()
+            driver.find_element_by_xpath(f'//*[@id="{self.alert_div_id}"]/div/div/div/div/div[3]/button[1]').click()
             sleep(random_delay())
             
-            driver.find_element_by_xpath('//*[@id="t--239073259"]/div/div/div/div/div[3]/button[2]').click()
+            driver.find_element_by_xpath(f'//*[@id="{self.alert_div_id}"]/div/div/div/div/div[3]/button[2]').click()
             sleep(random_delay())
 
         except:
@@ -108,8 +113,8 @@ class TinderBot():
     def smart_swipe(self):
         driver = self.driver
 
-        parent_xpath = '//*[@id="t-1801132545"]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[1]/div[3]/div[1]/div[1]/span[1]'
-        candidate_xpath = '//*[@id="t-1801132545"]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[1]/div[3]/div[1]/div[1]/span[1]/div'
+        parent_xpath = f'//*[@id="{self.main_div_id}"]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[1]/div[3]/div[1]/div[1]/span[1]'
+        candidate_xpath = f'//*[@id="{self.main_div_id}"]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[1]/div[3]/div[1]/div[1]/span[1]/div'
 
         while True:
             WebDriverWait(driver, 200).until(
@@ -120,13 +125,14 @@ class TinderBot():
                 EC.visibility_of_element_located((By.XPATH, candidate_xpath))
             )
 
+            self.handle_alerts()
+
             # Get parent span
             parent_span = driver.find_element_by_xpath(parent_xpath)
 
             # Get candidate element
             candidate_element = parent_span.find_element_by_xpath(candidate_xpath)
             self.rate_candidate(candidate_element)
-
 
     def rate_candidate(self, candidate_element):
         driver = self.driver
@@ -138,26 +144,46 @@ class TinderBot():
 
         candidate_image_url = parse_img_url_from_inline_style(candidate_inline_style)
 
-        # Download image
-        response = requests.get(candidate_image_url, stream=True)
-
-        candidate_img_filename = f"{candidate_name.replace(' ','_').lower()}.webp"
-
-        with open(f'images/{candidate_img_filename}', 'wb') as out_file:
-            shutil.copyfileobj(response.raw, out_file)
-
-        img_filename, prediction = predict_from_img_path(img_filename=candidate_img_filename)
+        prediction = predict_from_url_path(URL=candidate_image_url)
 
         print(f"\n\n[*] Puntuacion de {candidate_name}: {prediction}")
 
         if prediction > 2.9:
             # Like
-            print("[+++] LIKE <3 ")
-            driver.find_element_by_xpath('//*[@id="t-1801132545"]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[2]/div[4]/button').click()
+            print("[<3] LIKE ")
+            driver.find_element_by_xpath(f'//*[@id="{self.main_div_id}"]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[2]/div[4]/button').click()
             sleep(random_delay())
 
         else:
             # Nope
-            print("[----] NOPEEEE D:")
-            driver.find_element_by_xpath('//*[@id="t-1801132545"]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[2]/div[2]/button').click()
+            print("[D:] NOPE")
+            driver.find_element_by_xpath(f'//*[@id="{self.main_div_id}"]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[2]/div[2]/button').click()
             sleep(random_delay())
+
+    def handle_alerts(self):
+        driver = self.driver
+
+        # Tinder Passport alert - No thanks
+        passport_alert_nothanks_xpath = f'//*[@id="{self.alert_div_id}"]/div/div/div[1]/button/span'
+        
+        # UPGRADE YOUR LIKE - You just liked a popular profile...'No thanks' button
+        upgrade_like_nothanks_xpath = '/html/body/div[2]/div/div/button[2]'
+
+        if self.element_exists_by_xpath(passport_alert_nothanks_xpath):
+            driver.find_element_by_xpath(passport_alert_nothanks_xpath).click()
+            sleep(random_delay())
+
+        elif self.element_exists_by_xpath(upgrade_like_nothanks_xpath):
+            driver.find_element_by_xpath(upgrade_like_nothanks_xpath).click()
+            sleep(random_delay())
+
+        else:
+            pass
+
+    def element_exists_by_xpath(self, xpath):
+        try:
+            self.driver.find_element_by_xpath(xpath)
+        except NoSuchElementException:
+            return False
+
+        return True
--- use_model.py
+++ use_model.py
@@ -1,11 +1,17 @@
-from keras.models import load_model
-from keras.applications.resnet50 import preprocess_input
-from keras.preprocessing import image
-
 import numpy as np
 import os
 import pandas as pd
 import PIL
+import urllib
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+import tensorflow as tf
+
+from keras.models import load_model
+from keras.applications.resnet50 import preprocess_input
+from keras.preprocessing import image
+
+from io import BytesIO
 
 # Define paths
 local_path = os.path.abspath(os.getcwd())
@@ -34,4 +40,16 @@ def predict_from_img_path(img_foldername='images', img_filename='scarlett_johans
 
     return img_filename, prediction
 
-predict_from_img_path()
+def predict_from_url_path(URL):
+    with urllib.request.urlopen(URL) as url:
+        img = image.load_img(BytesIO(url.read()), target_size=(224, 224))
+
+    img = image.img_to_array(img)
+    img = preprocess_input(img)
+    img = np.expand_dims(img, axis=0)
+
+    prediction = model.predict(img)[0][0]
+
+    return prediction
+
+predict_from_url_path(URL='https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Scarlett_Johansson_by_Gage_Skidmore_2_%28cropped%2C_2%29.jpg/800px-Scarlett_Johansson_by_Gage_Skidmore_2_%28cropped%2C_2%29.jpg')
